2ca37b7db7b2c4c73ada42820f5b612f46efd2e3
rakhya <pravarakhya.chilukuri@gmail.com>
2018-02-10 19:00:11 -0500

    project started

---
+++ src/main/java/com/rakhya/messaging/activemqapp/ActivemqAppApplication.java
@@ -0,0 +1,12 @@
+package com.rakhya.messaging.activemqapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqAppApplication.class, args);
+	}
+}
